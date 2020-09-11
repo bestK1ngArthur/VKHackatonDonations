d05bc6c61381957b5810e3106c7978ba48eb69ea
Artem Belkov <bestk1ngarthur@aol.com>
2020-09-11 16:08:12 +0300

    Create project

---
+++ VKHackathonDonations/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  VKHackathonDonations
+//
+//  Created by Artem Belkov on 11.09.2020.
+//  Copyright © 2020 Tech Birds. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ VKHackathonDonations/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  VKHackathonDonations
+//
+//  Created by Artem Belkov on 11.09.2020.
+//  Copyright © 2020 Tech Birds. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
